7e48293b80ff4c41bd0f894a689f67e2cf560fa6
Swarnim <swarnim14.9@hotmail.com>
2025-09-29 10:21:35 +0530

    lean emacs config

--- init.el
+++ init.el
@@ -1,47 +1,107 @@
-;; Stop emacs from littering config file
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p custom-file)
-  (load custom-file))
+;;; init.el --- init file -*- lexical-binding: t; -*-
 
-;; Disable UI elements early in startup
-(tool-bar-mode -1)
+;;; Code:
+
+(setq inhibit-splash-screen t)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
 (scroll-bar-mode -1)
-
-;; Basic settings
-(setq-default show-trailing-whitespace t)
-(column-number-mode 1)
 (global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
-(add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font-16"))
-(set-frame-font "Iosevka Nerd Font-16" nil t)
-(setq inhibit-startup-screen t)
-(setq-default pixel-scroll-precision-mode t)
-(setq-default line-spacing 0.12)
-(setq make-backup-files nil)
-(setq create-lockfiles nil)
-(setq auto-save-default nil)
-(setq dired-dwim-target t)
-(setq-default indent-tabs-mode nil)
-;; (setq-default tab-width 4)
-(setq js-indent-level 2)
-(setq typescript-indent-level 2)
-(setq js-switch-indent-offset 2)
+(setq show-trailing-whitespace 1)
 
-;; Package management setup
+;; Package Setup
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-
-;; Ensure use-package is installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(unless package-archive-contents
+  (package-refresh-contents))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Line movement functions
+;; theme
+(use-package gruber-darker-theme)
+(load-theme 'gruber-darker t)
+
+
+;; magit
+(use-package magit
+  :commands (magit-status))
+
+;; which-key
+(use-package which-key
+  :config
+  (which-key-mode))
+
+;; consult
+(use-package consult)
+
+;; vertico
+(use-package vertico
+  :init
+  (vertico-mode))
+
+;; marginalia
+(use-package marginalia
+  :init
+  (marginalia-mode t))
+
+;; orderless
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+
+;; multiple-cursors
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)))
+
+;; auto treesitter config
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
+
+;; expand-region
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+;; apheleia
+(use-package apheleia
+  :config
+  (apheleia-global-mode +1))
+
+
+;; languages
+(use-package typescript-mode)
+(use-package go-mode)
+
+(use-package markdown-mode)
+(use-package yasnippet)
+
+;; lsp
+(add-to-list 'load-path "~/.emacs.d/lsp-bridge")
+
+(setq lsp-bridge-python-command "~/.emacs.d/lsp-bridge-env/bin/python")
+(require 'lsp-bridge)
+(global-lsp-bridge-mode)
+(setq lsp-bridge-breadcrumb-mode t)
+
+(with-eval-after-load 'lsp-bridge
+  (define-key lsp-bridge-mode-map (kbd "C-c l k") 'lsp-bridge-popup-documentation)
+  (define-key lsp-bridge-mode-map (kbd "C-c l r") 'lsp-bridge-rename)
+  (define-key lsp-bridge-mode-map (kbd "C-c l d") 'lsp-bridge-find-def)
+  (define-key lsp-bridge-mode-map (kbd "C-c l a") 'lsp-bridge-code-action)
+  (define-key lsp-bridge-mode-map (kbd "C-c l f") 'lsp-bridge-find-references)
+  )
+
+
 (defun move-line-up ()
   "Move up the current line."
   (interactive)
@@ -57,136 +117,32 @@
   (forward-line -1)
   (indent-according-to-mode))
 
-;;  Custom Keybindings
+;; Global keybindings
 (global-set-key (kbd "C-`") 'other-window)
-
 (global-set-key (kbd "M-m") 'compile)
 (global-set-key (kbd "M-i") 'back-to-indentation)
-
 (global-set-key (kbd "M-n") 'next-error)
 (global-set-key (kbd "M-p") 'previous-error)
-
-(global-set-key (kbd "M-<up>")  'move-line-up)
-(global-set-key (kbd "M-<down>")  'move-line-down)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Install and configure packages ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(use-package treesit-auto
-  :ensure t
-  :config
-  (global-treesit-auto-mode))
-
-(use-package htmlize
-  :ensure t)
-
-(use-package lsp-mode
-  :ensure t
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook
-  (tsx-ts-mode . lsp-deferred)
-  (jsx-ts-mode . lsp-deferred)
-  (typescript-ts-mode . lsp-deferred)
-  ;; Fallback modes
-  (typescript-mode . lsp-deferred)
-  (js-mode . lsp-deferred)
-
-  (js-ts-mode . lsp-deferred)
-  (html-mode . lsp-deferred)
-  :config
-  (setq lsp-signature-render-documentation nil)  ; Use nil instead of false
-  (setq lsp-log-io nil)        ; Disable logging if not needed
-  (setq lsp-ui-doc-enable t)                 ; Enable documentation popup
-  :commands lsp)
-
-(use-package lsp-ui :commands lsp-ui-mode)
-
-(use-package apheleia
-  :ensure t)
-(apheleia-global-mode +1)
-
-(use-package expand-region
-  :bind ("C-=" . er/expand-region))
-
-(use-package org
-  :ensure t
-  :config
-  ;; Basic settings
-  (setq org-startup-indented t)           ; Enable org-indent-mode by default
-  (setq org-startup-folded 'overview)     ; Start files folded
-  (setq org-return-follows-link t)        ; Make RET follow links
-  (setq org-hide-emphasis-markers t)      ; Hide formatting characters
-  (setq org-pretty-entities t)            ; Show entities as UTF8 characters
-  )
-
-(use-package gruber-darker-theme
-  :ensure t
-  :config
-  (load-theme 'gruber-darker t))
-
-;; (load-theme 'modus-vivendi)
-
-;; (use-package nerd-icons
-;;   :ensure t)
-
-;; (use-package doom-themes
-;;   :ensure t
-;;   :config
-;;   (setq doom-themes-enable-bold t
-;;         doom-themes-enable-italic t)
-;;   (load-theme 'doom-tokyo-night t)
-;;   (doom-themes-org-config)
-;;   )
-
-;; (use-package doom-modeline
-;;   :ensure t
-;;   :init (doom-modeline-mode 1))
-
-(use-package multiple-cursors
-  :ensure t
-  :bind (:map global-map
-              ("C-S-c C-S-c" . mc/edit-lines)
-              ("C->" . mc/mark-next-like-this)
-              ("C-<" . mc/mark-previous-like-this)
-              )
-  )
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
 
 
-(use-package eat
-  :ensure t)
 
-;; Install Magit from main branch
-(use-package magit
-  :ensure t)
-
-(use-package vertico
-  :ensure t
-  :init
-  (vertico-mode t))
-
-(use-package marginalia
-  :ensure t
-  :init
-  (marginalia-mode t))
-
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles . (partial-completion))))))
-
-;; Languages
-(use-package go-mode
-  :ensure t)
-
-(use-package typescript-mode
-  :after lsp-mode
-  :config
-  (add-to-list 'lsp-disabled-clients '(typescript-mode . typescript-language-server))
-  (add-to-list 'lsp-enabled-clients 'ts-ls))
-
-;; Performance optimizations
-(setq gc-cons-threshold 100000000)
-(setq read-process-output-max (* 1024 1024))
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(apheleia consult expand-region go-mode gruber-darker
+	      gruber-darker-theme magit marginalia markdown-mode
+	      multiple-cursors orderless treesit-auto typescript-mode
+	      vertico yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
